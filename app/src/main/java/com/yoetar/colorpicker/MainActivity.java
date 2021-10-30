d2f34375b971ba2429b135615c614cc1c69c9e71
ET-TALIBI YOUSSEF <et-talibiyoussef@ET-TALIBIs-MacBook-Pro.local>
2021-10-30 16:53:51 +0100

    Initial commit

---
+++ app/src/main/java/com/yoetar/colorpicker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yoetar.colorpicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
